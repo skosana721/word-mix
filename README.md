59df0f3963a62910de04495bd3950fe92a6693b1
Masekolg <masekolg@gmail.com>
2020-09-14 14:58:04 +0200

    add app.js file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # word-mix
+
+This function that remove certain number of words "Dum" before the first word of sentence, after the last word and between words 
---
+++ app.js
@@ -0,0 +1,8 @@
+const wordMix =(str)=>{
+let newStr = str.split('');
+ newStr.filter(function(a,b){
+    return str.indexOf(a) ===b
+})
+}
+
+console.log("DUBDUBIDUBAMDUBDUBX")
